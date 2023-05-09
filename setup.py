ddb7bb905eaf1658bbf90fb846d0381c9ce728cb
Hasan Gani Rachmatullah <ganirh0612@gmail.com>
2023-05-09 16:28:10 +0700

    revision of Readme
    Correcting Methode section

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="CNNtech_News",
-    version="0.1.2",
+    version="0.1.3",
     author="Hasan Gani Rachmatullah",
     author_email="ganirh0612@gmail.com",
     description="This package will provide you the most update of CNN Tech News",
